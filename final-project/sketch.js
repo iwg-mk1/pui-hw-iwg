6ef8da17f0fb8cc871861af606357e5aa73cb3ee
iwg-mk1 <86335822+iwg-mk1@users.noreply.github.com>
2024-11-17 21:21:51 -0500

    Working on getting the canvas up, still has bugs

--- final-project/sketch.js
+++ final-project/sketch.js
@@ -1,15 +1,47 @@
+let input;
+let img;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(100, 100);
+
+  // Create a file input and place it beneath
+  // the canvas.
+  input = createFileInput(handleImage);
+  input.position(0, 0);
+
+  describe('A gray square with a file input beneath it. If the user selects an image file to load, it is displayed on the square.');
 }
 
 function draw() {
-  background(220);
+  background(200);
+
+  // Draw the image if loaded.
+  if (img) {
+    resizeCanvas(img.width, img.height * 2);
+    image(img, 0, 0, img.width, img.height);
+    
+    for (let i = 0; i < img.pixels.length; i += 4) {
+      img.pixels[i] = img.pixels[i + 1];
+      img.pixels[i + 1] = img.pixels[i + 2];
+      img.pixels[i + 2] = img.pixels[i + 3];
+    }
+    img.updatePixels();
+    image(img, 0, img.height, img.width, img.height);
+  }
 }
 
 
-
-setpup();
-draw();
+// Create an image if the file is an image.
+function handleImage(file) {
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    img.loadPixels();
+    img.hide();
+  } else {
+    img = null;
+  }
+}
 
 
 /*
