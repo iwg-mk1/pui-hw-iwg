d699282c4f16527152b88d06bda182d2ce58d076
iwg-mk1 <86335822+iwg-mk1@users.noreply.github.com>
2024-12-08 23:46:21 -0500

    fixed bug where array was being accessed while it was being modified by an async

--- final-project-test/script.js
+++ final-project-test/script.js
@@ -12,39 +12,40 @@ const colorPalette = [
     [0, 0, 0, null, 0]
 ];
 
-//palettePush([Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]);
+
+originalImage.style.display = 'none';
+filteredImage.style.display = 'none';
+addColorButton.style.display = 'none';
+document.getElementById('dimensions').style.display = 'none';
 
 
 
 function addColorToPalette(event) {
     const imgRect = originalImage.getBoundingClientRect();
-  //const scaleX = originalImage.width / imgRect.width;
-  //const scaleY = originalImage.height / imgRect.height;
+    //const scaleX = originalImage.width / imgRect.width;
+    //const scaleY = originalImage.height / imgRect.height;
 
-  // Create a canvas to resize the image
-  const resizeCanvas = document.createElement('canvas');
-  const resizeCtx = resizeCanvas.getContext('2d');
-  resizeCanvas.width = imgRect.width;
-  resizeCanvas.height = imgRect.height;
-  resizeCtx.drawImage(originalImage, 0, 0, imgRect.width, imgRect.height);
+    // Create a canvas to resize the image
+    const resizeCanvas = document.createElement('canvas');
+    const resizeCtx = resizeCanvas.getContext('2d');
+    resizeCanvas.width = imgRect.width;
+    resizeCanvas.height = imgRect.height;
+    resizeCtx.drawImage(originalImage, 0, 0, imgRect.width, imgRect.height);
 
-  const x = event.clientX - imgRect.left;
-  const y = event.clientY - imgRect.top;
+    const x = event.clientX - imgRect.left;
+    const y = event.clientY - imgRect.top;
 
-  const pixelData = resizeCtx.getImageData(x, y, 1, 1).data;
-  const newColor = [pixelData[0], pixelData[1], pixelData[2]];
+    const pixelData = resizeCtx.getImageData(x, y, 1, 1).data;
+    const newColor = [pixelData[0], pixelData[1], pixelData[2]];
 
-  palettePush(newColor);
+    palettePush(newColor);
 
-  }
-  
+}
 
-  originalImage.style.display = 'none';
-  filteredImage.style.display = 'none';
-  addColorButton.style.display = 'none';
-  document.getElementById('dimensions').style.display = 'none';
 
-  function applyFilter() {
+
+
+function applyFilter() {
 
     document.getElementById('info-box').style.display = 'none';
     addColorButton.style.display = 'block';
@@ -54,61 +55,62 @@ function addColorToPalette(event) {
 
     const file = imageInput.files[0];
     const reader = new FileReader();
-  
+
     reader.onload = (event) => {
-      const img = new Image();
-      img.src = event.target.result;
-  
-      img.onload
-   = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-  
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  
-        const data = imageData.data;
-        
-        //Reseting the count
-        for(let i = 0; i < colorPalette.length; i++) {
-            colorPalette[i][4] = 0
-        }
-  
-        for (let i = 0; i < data.length; i += 4) {
-          const r = data[i];
-          const g = data[i + 1];
-          const b = data[i + 2];
-  
-          let minDistance = Infinity;
-          let closestColorIndex = 0;
-  
-          for (let j = 0; j < colorPalette.length; j++) {
-            const [pr, pg, pb] = colorPalette[j];
-            const distance = Math.sqrt((r - pr) ** 2 + (g - pg) ** 2 + (b - pb) ** 2);
-            if (distance < minDistance) {
-              minDistance = distance;
-              closestColorIndex = j;
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = () => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+            const data = imageData.data;
+
+            //Reseting the count
+            for (let i = 0; i < colorPalette.length; i++) {
+                colorPalette[i][4] = 0;
             }
-          }
-  
-          const [cr, cg, cb] = colorPalette[closestColorIndex];
-          colorPalette[closestColorIndex][4] += 1; //Adding to the count
-          data[i] = cr;
-          data[i + 1] = cg;
-          data[i + 2] = cb;
-        }
-  
-        ctx.putImageData(imageData, 0, 0);
-        filteredImage.src = canvas.toDataURL();
-        originalImage.src = event.target.result;
-      };
+
+            for (let i = 0; i < data.length; i += 4) {
+                const r = data[i];
+                const g = data[i + 1];
+                const b = data[i + 2];
+
+                let minDistance = Infinity;
+                let closestColorIndex = 0;
+
+                for (let j = 0; j < colorPalette.length; j++) {
+                    const [pr, pg, pb] = colorPalette[j];
+                    const distance = Math.sqrt((r - pr) ** 2 + (g - pg) ** 2 + (b - pb) ** 2);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        closestColorIndex = j;
+                    }
+                }
+
+                const [cr, cg, cb] = colorPalette[closestColorIndex];
+                colorPalette[closestColorIndex][4] += 1; //Adding to the count
+                data[i] = cr;
+                data[i + 1] = cg;
+                data[i + 2] = cb;
+            }
+
+            ctx.putImageData(imageData, 0, 0);
+            filteredImage.src = canvas.toDataURL();
+            originalImage.src = event.target.result;
+
+            calculateColorAreas();
+        };
     };
-  
+
     reader.readAsDataURL(file);
-    calculateColorAreas();
-  }
+
+}
 
 
 imageInput.addEventListener('change', applyFilter);
@@ -116,12 +118,12 @@ imageInput.addEventListener('change', applyFilter);
 addColorButton.addEventListener('click', addColor);
 
 
-function addColor (event) {
+function addColor(event) {
     palettePush([Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]);
 }
 
 
-function palettePush ([r,g, b]) {
+function palettePush([r, g, b]) {
 
     const entry = document.createElement('div');
     entry.classList.add('palette-entry');
@@ -129,7 +131,7 @@ function palettePush ([r,g, b]) {
     let colorPicker = document.createElement('input');
     colorPicker.type = 'color';
     colorPicker.value = rgbToHex(r, g, b);
-    colorPicker.addEventListener("change", updateColors, false); //Change to 'change' for changing when exiting
+    colorPicker.addEventListener("change", updateColors, false);
     colorPicker.select();
     entry.appendChild(colorPicker);
 
@@ -158,47 +160,37 @@ function palettePush ([r,g, b]) {
     removeButton.innerText = 'Remove Color';
     removeButton.classList.add('remove-button');
     entry.appendChild(removeButton);
-    removeButton.addEventListener('click', (event) => 
-        {
-            removeColorFromPalette(entry);
-            entry.remove();
-        });
-    
+    removeButton.addEventListener('click', (event) => {
+        removeColorFromPalette(entry);
+        entry.remove();
+        applyFilter();
+    });
+
 
     paletteContainer.appendChild(entry);
 
     colorPalette.push([r, g, b, entry, 0]);
     applyFilter();
 }
-// color, name, sqft, cans, gallons
+
 
 function removeColorFromPalette(entry) {
     for (let i = 0; i < colorPalette.length; i++) {
         if (entry === colorPalette[i][3]) {
-            colorPalette.splice(i, 1)
-            applyFilter();
+            colorPalette.splice(i, 1);
             break;
         }
-        
+
     }
 }
 
 
 function updateColors(event) {
 
-    //this.parentElement.querySelector('.color-name').innerText = event.target.value;
-    //console.log(getColorName(event.target.value).then(result => {return result;}));
-    //this.parentElement.querySelector('.color-name').innerText = getColorName(event.target.value).then(result => {return result;});
-
     getColorName(event.target.value).then(result => {
         this.parentElement.querySelector('.color-name').innerText = result
     });
 
-
-
-    //const c = getColorName('#FF0000').then((result) => {return result;});
-
-  //console.log(c);
     for (let i = 0; i < colorPalette.length; i++) {
         if (this.parentElement === colorPalette[i][3]) {
             let newColor = hexToRgb(event.target.value);
@@ -208,65 +200,62 @@ function updateColors(event) {
             applyFilter();
             break;
         }
-        
+
     }
 
 }
 
 
-function calculateColorAreas () {
+function calculateColorAreas() {
 
     let area = w * h;
     let totalColors = 0;
-    for(let i = 0; i < colorPalette.length; i++) {
+    for (let i = 0; i < colorPalette.length; i++) {
         totalColors += colorPalette[i][4];
     }
-    
-    for(let i = 0; i < document.getElementById('paletteContainer').children.length; i++) {
+
+
+    for (let i = 0; i < document.getElementById('paletteContainer').children.length; i++) {
         const entry = document.getElementById('paletteContainer').children[i];
-        
-        let colorArea = area * colorPalette[i][4] / totalColors;
+        console.log(entry);
+        let colorArea = area * colorPalette[i + 1][4] / totalColors;
         entry.querySelector('.sqft').innerText = colorArea.toFixed(1) + ' sqft';
         entry.querySelector('.cans').innerText = (colorArea / 25).toFixed(1) + ' cans';
         entry.querySelector('.gallons').innerText = (colorArea / 350).toFixed(1) + ' gallons';
-        
+
     }
 }
 
 widthField.addEventListener('change', (event) => {
-    
-    if(event.target.value >= 0) {
-        
+
+    if (event.target.value >= 0) {
         w = event.target.value;
         calculateColorAreas();
     }
-}) 
+})
 
 heightField.addEventListener('change', (event) => {
-
-    if(event.target.value >= 0) {
+    if (event.target.value >= 0) {
         h = event.target.value;
         calculateColorAreas();
     }
-}) 
+})
 
 
 async function getColorName(hexCode) {
     const url = 'https://api.color.pizza/v1/?values=' + hexCode.slice(1) + '&list=xkcd';
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      //console.log(json);
-      //console.log(json.paletteTitle)
-      return json.paletteTitle;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json.paletteTitle;
     } catch (error) {
-      console.error(error.message);
+        console.error(error.message);
     }
-  }
+}
 
 
 
@@ -275,30 +264,17 @@ function rgbToHex(r, g, b) {
 }
 
 function hexToRgb(hex) {
-    // Remove the # if present
     hex = hex.replace("#", "");
-  
-    // Check if it's a 3-digit hex code
+
     if (hex.length === 3) {
-      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
     }
-  
-    // Parse the hex values into decimal
+
     const r = parseInt(hex.slice(0, 2), 16);
     const g = parseInt(hex.slice(2, 4), 16);
     const b = parseInt(hex.slice(4, 6), 16);
-  
+
     return [r, g, b];
 }
 
 
-  /*
-  function colorDistance(r, g, b, pr, pg, pb)
-{
-    let rmean = (r + pr) >> 1;
-    let r1 = r - pr;
-    let g1 = g - pg;
-    let b1 = b - pb;
-    return Math.sqrt((((512+rmean)*r1*r1)>>8) + 4*g1*g1 + (((767-rmean)*b1*b1)>>8));
-}
-    */
